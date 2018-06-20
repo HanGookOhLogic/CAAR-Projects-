dac8f6e78714512ad6e90ea488f839bea987acf1
Dan <dshofman0@gmail.com>
2018-06-20 10:20:02 -0400

    code updates and modularization

---
+++ pairs_helper.py
@@ -0,0 +1,46 @@
+import math
+
+# Generates a list of pairs of pixels containing points a distance of 1 from each other
+# Input: s - length of side of grid of pixels
+#        n - number of pixels on each side of grid (nxn grid)
+#        wrapping - set to true if we want distances to wrap across edges of grid as if grid was tiled
+def list_of_pixel_pairs(s, n, wrapping):
+    pairs = [[[] for i in range(n)] for j in range(n)] # nxn array of lists of points distance 1 from each pixel
+    pixel_length = float(s)/n # length of each pixel
+  
+    for i in range(0, int(math.ceil(float(n)/s + 1))):
+        for j in range(0, int(math.ceil(float(n)/s + 1))):
+            min_dist = 0
+            max_dist = 0
+            # calculate the minimum and maximum distance between a point in pixel (0, 0) and pixel (s/n*i, s/n*j)
+            if i == 0:
+                min_dist = pixel_length*(j-1)
+                max_dist = pixel_length*(j+1)
+            elif j == 0:
+                min_dist = pixel_length*(i-1)
+                max_dist = pixel_length*(i+1)
+            else:
+                min_dist = math.hypot(pixel_length*(i-1), pixel_length*(j-1))
+                max_dist = math.hypot(pixel_length*(i+1), pixel_length*(j+1))
+        
+            # if 1 is between the distances, then by continuity, there is a pair of points a distance of 1 in this pair of pixels
+            if min_dist <= 1 and 1 <= max_dist:
+                # by symmetry of our pixel division, the pixel pair we found can be applied to any starting pixel, not just (0,0) and we can flip the pair vertically and horizontally as well
+                for x in range(n):
+                    for y in range(n):
+                        if wrapping:
+                            pairs[x][y].append(((x+i)%n, (y+j)%n))
+                            pairs[(x+i)%n][(y+j)%n].append((x, y))
+                            if i != 0 and j != 0:
+                                pairs[x][y].append(((x-i)%n, (y+j)%n))
+                                pairs[(x-i)%n][(y+j)%n].append((x, y))
+                        else:
+                            if y + j < n:
+                                if x + i < n:
+                                    pairs[x][y].append((x+i, y+j))
+                                    pairs[x+i][y+j].append((x, y))
+                                if x - i >= 0 and i != 0 and j != 0:
+                                    pairs[x][y].append((x-i, y+j))
+                                    pairs[x-i][y+j].append((x, y))
+                    
+    return pairs
