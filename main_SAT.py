37ebfc8bd0c513ae90b12730ceb030706403e23f
Dan <dshofman0@gmail.com>
2018-06-20 16:28:17 -0400

    SAT iter

--- main_SAT.py
+++ main_SAT.py
@@ -3,21 +3,39 @@ import printColourGrid
 import numpy
 
 # CONSTANTS
-S = 4 # Side length of nxn square
-N = 14 # Number of pixels on one side of square
-K = 7 # Number of colors
+S = 1.5 # Side length of nxn square
+N = 2 # Number of pixels on one side of square
+K = 4 # Number of colors
 WRAPPING = False # Whether or not we wrap distances across edges of square when finding pairs of pixels unit distaqnce from each other
+ITER = False # Whether to find search for every solution. If ITER = False, the program terminates as soon as the first solution is found
 
 
-coloring = sat_solver.SAT_solve(S, N, K, WRAPPING)
-if coloring == "UNSAT":
-    print "This coloring problem is unsatisfiable."
+wrappingString = 'False'
+if WRAPPING:
+    wrappingString = 'True'
+
+folderString = 'SAT/'
+if ITER:
+    folderString = 'SAT_iter/'
+    
+name = folderString + 's' + (str(S)).replace('.',',') + '_n' + str(N) + '_k' + str(K) + '_wrapping' + wrappingString
+
+if ITER:
+    colorings = sat_solver.SAT_itersolve(S, N, K, WRAPPING)
+    if not colorings:
+        print "This coloring problem is unsatisfiable."
+    else:
+        name += '_iter'
+        i = 0
+        for coloring in colorings:
+            printColourGrid.create_coloring(coloring, N, K, name + str(i))
+            i += 1
 else:
-    wrappingString = 'False'
-    if WRAPPING:
-        wrappingString = 'True'
-    name = 'SAT/s' + (str(S)).replace('.',',') + '_n' + str(N) + '_k' + str(K) + '_wrapping' + wrappingString
-    printColourGrid.create_coloring(coloring, N, K, name)
+    coloring = sat_solver.SAT_solve(S, N, K, WRAPPING)
+    if coloring == "UNSAT":
+        print "This coloring problem is unsatisfiable."
+    else:
+        printColourGrid.create_coloring(coloring, N, K, name)
 
 
 """
